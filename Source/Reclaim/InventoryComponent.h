072cead206c9bb27ee4b3c3d91125507dcd57f1a
Darren White <74544109+Dayron124@users.noreply.github.com>
2025-07-11 20:56:17 +0100

    feature (inventory): Add InventoryComponent with basic AddItem functionality

--- Source/Reclaim/InventoryComponent.h
+++ Source/Reclaim/InventoryComponent.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright [Your Studio Name] 2025. All Rights Reserved.
 
 #pragma once
 
@@ -6,23 +6,57 @@
 #include "Components/ActorComponent.h"
 #include "InventoryComponent.generated.h"
 
+/**
+ * Defines the data for a single item in the inventory.
+ */
+USTRUCT(BlueprintType)
+struct FInventoryItem
+{
+	GENERATED_BODY()
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+public:
+	// Expose ItemID to be set when creating the struct in Blueprints.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FName ItemID;
+
+	// Expose DisplayName to be set when creating the struct in Blueprints.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FString DisplayName;
+
+	// The current quantity of this item stack.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	int32 Quantity;
+
+	// Default constructor
+	FInventoryItem() : ItemID(NAME_None), Quantity(0) {}
+};
+
+/**
+ * Manages a collection of items for an actor. Can be added to players, containers, etc.
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class RECLAIM_API UInventoryComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UInventoryComponent();
+
+	/**
+	 * Adds a new item to the inventory.
+	 * @param NewItem The item to add.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void AddItem(const FInventoryItem& NewItem);
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	// The array of items currently held in this inventory.
+	UPROPERTY(VisibleAnywhere, Category = "Inventory")
+	TArray<FInventoryItem> Items;
 
-		
 };
