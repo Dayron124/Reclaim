df2cd8ca7c31da1e0d1a1ddd299c70062dcdf318
Darren <darrenkeithwhite123@gmail.com>
2025-07-11 19:39:26 +0100

    Configure Git LFS tracking

---
+++ Source/Reclaim/Reclaim.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Reclaim : ModuleRules
+{
+	public Reclaim(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Reclaim",
+			"Reclaim/Variant_Platforming",
+			"Reclaim/Variant_Combat",
+			"Reclaim/Variant_Combat/AI",
+			"Reclaim/Variant_SideScrolling",
+			"Reclaim/Variant_SideScrolling/Gameplay",
+			"Reclaim/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
